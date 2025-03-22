817a0352eee8b0432890a467e40fa7ba334c3759
Renato <rnt.graca01@gmail.com>
2025-03-21 23:19:01 -0300

    feat: Documentação da api com swagger openapi

--- src/main/java/com/proposta/app/adapters/inbounds/models/PropostaRequest.java
+++ src/main/java/com/proposta/app/adapters/inbounds/models/PropostaRequest.java
@@ -1,12 +1,14 @@
 package com.proposta.app.adapters.inbounds.models;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record PropostaRequest(
-        String nome,
-        String sobrenome,
-        String telefone,
-        String cpf,
-        Double renda,
-        Double valorSolicitado,
-        int prazoPagamento
+        @Schema(description = "Primeiro Nome do cliente solicitante", example = "Fulano") String nome,
+        @Schema(description = "Sobrenome do cliente solicitante", example = "de Beltrano") String sobrenome,
+        @Schema(description = "Telefone do cliente solicitante", example = "5511912345678") String telefone,
+        @Schema(description = "CPF do cliente solicitante", example = "12345678901") String cpf,
+        @Schema(description = "Renda do cliente solicitante", example = "1000.00") Double renda,
+        @Schema(description = "Valor solicitado para empréstimo", example = "1000.00") Double valorSolicitado,
+        @Schema(description = "Quantidade de meses para pagamento", example = "12") int prazoPagamento
 ) {
 }
--- src/main/java/com/proposta/app/adapters/inbounds/models/PropostaResponse.java
+++ src/main/java/com/proposta/app/adapters/inbounds/models/PropostaResponse.java
@@ -1,15 +1,17 @@
 package com.proposta.app.adapters.inbounds.models;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record PropostaResponse(
-        long id,
-        String nome,
-        String sobrenome,
-        String telefone,
-        String cpf,
-        Double renda,
-        String valorSolicitadoFmt,
-        int prazoPagamento,
-        Boolean aprovada,
-        String observacao
+        @Schema(description = "ID da proposta criada", example = "1")long id,
+        @Schema(description = "Primeiro Nome do cliente solicitante", example = "Fulano") String nome,
+        @Schema(description = "Sobrenome do cliente solicitante", example = "de Beltrano") String sobrenome,
+        @Schema(description = "Telefone do cliente solicitante", example = "5511912345678") String telefone,
+        @Schema(description = "CPF do cliente solicitante", example = "12345678901") String cpf,
+        @Schema(description = "Renda do cliente solicitante", example = "1000") Double renda,
+        @Schema(description = "Valor solicitado para empréstimo", example = "R$ 1000,00")String valorSolicitadoFmt,
+        @Schema(description = "Prazo de pagamento do empréstimo em meses", example = "10") int prazoPagamento,
+        @Schema(description = "Status da proposta", example = "true") Boolean aprovada,
+        @Schema(description = "Motivo recusa da proposta", example = "Proposta recusada por...") String observacao
 ) {
 }
